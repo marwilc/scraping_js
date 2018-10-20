0fd5464e531e8e97f358cc951b531afe0b586c1b
Marwil Campos <marwilcampos@gmail.com>
2018-10-20 09:10:32 -0400

    no scripts

--- index.js
+++ index.js
@@ -11,17 +11,55 @@ request('http://codedemos.com/sampleblog', (error,
     response, html) => {
     if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
-        const body = $('html').html();
+        var obj = {};
+        //var body = document.body;
+
+        const body = $('html');
+        console.log(body[0]);
         var json;
-        json = himalaya.parse(html);
-        console.dir(json, {colors: true, depth: null});
+        //json = himalaya.parse(html);
+        //console.dir(json, {colors: true, depth: null});
+        getAllChild(body[0], obj);
 
         var fs = require('fs');
-        fs.writeFile("test.json", JSON.stringify(json, null, 4), function (err) {
+        fs.writeFile("test.json", JSON.stringify(obj, null, 4), function (err) {
             if (err) {
                 console.log(err);
             }
         });
+
     }
 });
+
+
+function getAllChild(element, obj) {
+    if (element.type != 'script' && element.type != 'style') {
+        //obj.type = element.type;
+        //obj.name = element.name;
+        if(element.attribs != undefined ){
+            obj.attribs = element.attribs;
+
+        }
+        if(element.content != undefined){
+            obj.content = element.content;
+        }
+            
+            
+        //obj.parent = element.parent();
+        console.log(obj);
+        if (element.children) {
+            if (element.children.length != 0) {
+                obj.children = [];
+                [].forEach.call(element.children, (val, i) => {
+                    obj.children[i] = {};
+                    getAllChild(val, obj.children[i]);
+                });
+            }
+
+        }
+
+    }
+
+
+}
 
